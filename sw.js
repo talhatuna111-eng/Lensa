923e241cc34c892dddd716800bbe58f28e3a9d52
talhatuna111-eng <talhatuna111@gmail.com>
2026-08-04 13:32:50 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lensa-pwa-v6-ai-server';
+const CACHE_NAME = 'lensa-pwa-v7-no-puter';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
